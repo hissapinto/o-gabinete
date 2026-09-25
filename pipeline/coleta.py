46d8719e5c504834fff9c305dd4d57b457879b52
kaiqpaiva <kaique.paiva681@gmail.com>
2026-09-25 16:41:52 -0300

    feat: adiciona URL de proposições e atualiza coleta de dados

--- pipeline/coleta.py
+++ pipeline/coleta.py
@@ -6,12 +6,13 @@ ANO_REFERENCIA = 2024
 URL_VOTOS = f'https://dadosabertos.camara.leg.br/arquivos/votacoesVotos/csv/votacoesVotos-{ANO_REFERENCIA}.csv'
 URL_VOTACOES = f'https://dadosabertos.camara.leg.br/arquivos/votacoes/csv/votacoes-{ANO_REFERENCIA}.csv'
 URL_OBJETOS = f'https://dadosabertos.camara.leg.br/arquivos/votacoesObjetos/csv/votacoesObjetos-{ANO_REFERENCIA}.csv'
+URL_PROPOSICOES = f'https://dadosabertos.camara.leg.br/arquivos/votacoesProposicoes/csv/votacoesProposicoes-{ANO_REFERENCIA}.csv'
 
 def baixar_dados_brutos():
     pasta = RAIZ / "dados" / "brutos"
     pasta.mkdir(parents=True, exist_ok=True)
 
-    for url in [URL_VOTOS, URL_VOTACOES, URL_OBJETOS]:
+    for url in [URL_VOTOS, URL_VOTACOES, URL_OBJETOS, URL_PROPOSICOES]:
         nome_arquivo = url.split('/')[-1]
         caminho_arquivo = pasta / nome_arquivo
 
